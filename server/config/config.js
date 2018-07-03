a4f54cb36b359214686c07fcdabad9fed5a11633
Guillermo Solano <ing.guillermosolano@gmail.com>
2018-07-02 21:55:02 -0500

    Rest Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,16 @@
 
 // Puerto 
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDb 
+
+if ( process.env.NODE_ENV === 'dev' ) {
+    urlDb = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDb = 'mongodb://gllrm:andr3519@ds125871.mlab.com:25871/cafe-gllrm';
+}
+
+process.env.urlDB = urlDb
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 require('./config/config')
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express()
 const bodyParser = require('body-parser');
 
@@ -11,27 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('../routes/usuario'))
 
-app.get('/usuario', function( req, res ) {
-    res.json('Hello world');
+
+mongoose.connect( process.env.urlDB, ( err, res ) => {
+    if ( err ) throw err;
+    console.log('Base de datos ONLINE');
+    
 });
 
-app.post('/usuario', function( req, res ) {
-
-    let body = req.body
-    res.json({body});
-});
-
-app.put('/usuario/:id', function( req, res ) {
-
-    let id = req.params.id;
-
-    res.json({ id });
-});
-
-app.delete('/usuario', function( req, res ) {
-    res.json('Hello world');
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto: ', process.env.PORT);
